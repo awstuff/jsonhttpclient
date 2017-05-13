be7bd6da9fcd369590ea93f1da4a5d172dc6c0c5
Adrian Wirth <adrian.wirth@yahoo.de>
2017-05-13 09:03:12 +0200

    Update HttpResponseCodeManager.java

--- HttpResponseCodeManager.java
+++ HttpResponseCodeManager.java
@@ -9,7 +9,6 @@ import java.util.Set;
 /**
  * A class for registering event listeners for certain HTTP response codes.
  * For use with JsonHttpClient
- * <p/>
  * Copyright 2016 Adrian Wirth
  * Released under the MIT license
  */
